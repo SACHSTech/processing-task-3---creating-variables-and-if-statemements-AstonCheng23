1e324e26a51fa9a8ed917cc2a25df2dc4708a8af
AstonCheng23 <aston.cheng23@ycdsbk12.ca>
2022-03-04 12:23:25 -0500

    made things, currently trying to fix if loop

--- Sketch.java
+++ Sketch.java
@@ -1,14 +1,24 @@
+import java.util.Random;
+
 import processing.core.PApplet;
 
 public class Sketch extends PApplet {
 	
+  float xDraw = 0; 
+
+  float xStartPos = random(0, 1600);
+  float yStartPos = random(0, 800);
+
+  int colour = 0;
+
+  boolean limit = true;
 	
   /**
    * Called once at the beginning of execution, put your size all in this method
    */
   public void settings() {
 	// put your size call here
-    size(400, 400);
+    size(1600, 800);
   }
 
   /** 
@@ -16,21 +26,33 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    background(100, 100, 100);
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
 
-    stroke(255);
-    line(50, 125, 70, 50);  
-  }
+    xDraw ++;
+    double yDraw = 0.005*(xDraw - xStartPos)*(xDraw - xStartPos) ;
+
+    stroke(colour);
+    line(xStartPos, yStartPos, (float)xDraw, (float)yDraw);  
+
+//    stroke(0);
+//    line((float)xDraw, (float)yDraw, (float)xDraw, (float)yDraw);
+
+    if (colour > 255 && limit == true){
+      limit = false;
+    } else if (colour < 1 && limit == false){
+      limit = true;
+    } else if (limit = true){
+      colour ++;
+    } else if (limit = false){
+      colour --;
+    }
   
   // define other methods down here.
+  }
 }
