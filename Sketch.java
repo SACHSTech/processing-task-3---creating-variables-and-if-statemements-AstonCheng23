78f134a1be29d660b25199de2d1e6d9621426195
AstonCheng23 <aston.cheng23@ycdsbk12.ca>
2022-03-07 14:27:17 -0500

    trying to make time stuff

--- Sketch.java
+++ Sketch.java
@@ -6,19 +6,20 @@ public class Sketch extends PApplet {
 	
   float xDraw = 0; 
 
-  float xStartPos = random(0, 1600);
+  float xStartPos = random(0, 800);
   float yStartPos = random(0, 800);
 
-  int colour = 0;
-
   boolean limit = true;
-	
+  boolean stop = false;
+  
+  int radius = 100;
+  double inversion = 0.01;
   /**
    * Called once at the beginning of execution, put your size all in this method
    */
   public void settings() {
 	// put your size call here
-    size(1600, 800);
+    size(800, 800);
   }
 
   /** 
@@ -26,7 +27,7 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(100, 100, 100);
+    background(255, 255, 255);
   }
 
   /**
@@ -34,25 +35,61 @@ public class Sketch extends PApplet {
    */
   public void draw() {
 
-    xDraw ++;
-    double yDraw = 0.005*(xDraw - xStartPos)*(xDraw - xStartPos) ;
+    int s = second();  // Values from 0 - 59
+    int m = minute();  // Values from 0 - 59
+    int h = hour();    // Values from 0 - 23
+    //copied straight from https://processing.org/reference/second_.html
+    
+    
+    stroke(0);
+    line(800/s, 0, 800/s, 33);
+    
+    stroke(0);
+    text(s, xStartPos, yStartPos);
 
-    stroke(colour);
+    stroke(0);
+    text(s, s, 33/2);
+
+    line(m, 33, m, 66);
+    line(h, 66, h, 100);
+
+    if (s >= 30){
+      inversion = 0.01;
+    } else if (s < 30){
+      inversion = -0.01;
+    }
+
+    xDraw ++;
+    double yDraw = inversion*(xDraw - xStartPos)*(xDraw - xStartPos);
+
+    int colour = (int)(100*Math.sin(xDraw) + 100);
+
+    stroke(colour, colour, 255);
     line(xStartPos, yStartPos, (float)xDraw, (float)yDraw);  
 
-//    stroke(0);
-//    line((float)xDraw, (float)yDraw, (float)xDraw, (float)yDraw);
+    stroke(0);
+    line((float)xDraw, (float)yDraw, (float)xDraw, (float)yDraw);
 
+/*
     if (colour > 255 && limit == true){
       limit = false;
     } else if (colour < 1 && limit == false){
       limit = true;
+    } 
+    
+    if (limit = false || limit != true){
+      colour --;
     } else if (limit = true){
       colour ++;
-    } else if (limit = false){
-      colour --;
     }
-  
+*/
   // define other methods down here.
+
+    if (xDraw > 800 || yDraw < 0) {
+      stop = true;
+        while (stop == true)
+          xDraw = 0;
+
+    }
   }
 }
