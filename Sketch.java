9ab1a88f285a0467184103c85bc5ad1fed4bd30f
AstonCheng23 <aston.cheng23@ycdsbk12.ca>
2022-03-07 19:38:15 -0500

    saving

--- Sketch.java
+++ Sketch.java
@@ -1,5 +1,3 @@
-import java.util.Random;
-
 import processing.core.PApplet;
 
 public class Sketch extends PApplet {
@@ -27,7 +25,7 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(255, 255, 255);
+    background(0, 0, 0);
   }
 
   /**
@@ -41,34 +39,27 @@ public class Sketch extends PApplet {
     //copied straight from https://processing.org/reference/second_.html
     
     
+    stroke(250);
+    line(s*13, 0, s*13, 33);
+
     stroke(0);
-    line(800/s, 0, 800/s, 33);
+    text(s, s*14, 33);
     
-    stroke(0);
-    text(s, xStartPos, yStartPos);
+    stroke(250);
+    line(m*13, 33, m*13, 66);
+    
+    stroke(250);
+    line(h*12, 66, h*12, 100);
 
-    stroke(0);
-    text(s, s, 33/2);
-
-    line(m, 33, m, 66);
-    line(h, 66, h, 100);
-
-    if (s >= 30){
-      inversion = 0.01;
-    } else if (s < 30){
-      inversion = -0.01;
-    }
 
     xDraw ++;
     double yDraw = inversion*(xDraw - xStartPos)*(xDraw - xStartPos);
 
-    int colour = (int)(100*Math.sin(xDraw) + 100);
-
-    stroke(colour, colour, 255);
+    stroke((float)xDraw*s, (float)yDraw*s, (float)s);
     line(xStartPos, yStartPos, (float)xDraw, (float)yDraw);  
 
-    stroke(0);
-    line((float)xDraw, (float)yDraw, (float)xDraw, (float)yDraw);
+//    stroke(0);
+//    line((float)xDraw, (float)yDraw, (float)xDraw, (float)yDraw);
 
 /*
     if (colour > 255 && limit == true){
@@ -82,14 +73,14 @@ public class Sketch extends PApplet {
     } else if (limit = true){
       colour ++;
     }
-*/
-  // define other methods down here.
 
+  // define other methods down here.
     if (xDraw > 800 || yDraw < 0) {
       stop = true;
         while (stop == true)
           xDraw = 0;
-
     }
+*/
+
   }
 }
