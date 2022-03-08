3ccf06d2b701c724f48897a75bd51e4768440134
AstonCheng23 <aston.cheng23@ycdsbk12.ca>
2022-03-07 22:17:44 -0500

    saving

--- Sketch.java
+++ Sketch.java
@@ -12,25 +12,14 @@ public class Sketch extends PApplet {
   
   int radius = 100;
   double inversion = 0.01;
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
+
   public void settings() {
-	// put your size call here
     size(800, 800);
   }
-
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
   public void setup() {
     background(0, 0, 0);
   }
 
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
   public void draw() {
 
     int s = second();  // Values from 0 - 59
@@ -38,49 +27,45 @@ public class Sketch extends PApplet {
     int h = hour();    // Values from 0 - 23
     //copied straight from https://processing.org/reference/second_.html
     
-    
-    stroke(250);
-    line(s*13, 0, s*13, 33);
-
     stroke(0);
-    text(s, s*14, 33);
+    text(s, s * 13, 33);
     
-    stroke(250);
-    line(m*13, 33, m*13, 66);
+    stroke(255);
+    text(m, m * 13, 66);
     
-    stroke(250);
-    line(h*12, 66, h*12, 100);
+    stroke(255);
+    text(h, h * 33, 99);
 
-
+    // why is this making other text commands black?
+/*    if (s == 59){
+      stroke(0, 0, 0);
+      fill(0, 0, 0);
+      rect(0, 0, 800, 33);
+    }
+*/
     xDraw ++;
     double yDraw = inversion*(xDraw - xStartPos)*(xDraw - xStartPos);
 
-    stroke((float)xDraw*s, (float)yDraw*s, (float)s);
-    line(xStartPos, yStartPos, (float)xDraw, (float)yDraw);  
-
-//    stroke(0);
-//    line((float)xDraw, (float)yDraw, (float)xDraw, (float)yDraw);
-
-/*
-    if (colour > 255 && limit == true){
-      limit = false;
-    } else if (colour < 1 && limit == false){
-      limit = true;
-    } 
-    
-    if (limit = false || limit != true){
-      colour --;
-    } else if (limit = true){
-      colour ++;
+    if (m < 20) {
+      stroke((float) xDraw * s, (float) yDraw * s, (float) s);
+    }
+    if (m >= 20 && m <= 40) {
+      stroke((float) s, (float) xDraw * s, (float) yDraw * s);
+    }
+    if (m > 40) {
+      stroke((float) yDraw * s, (float) s, (float) xDraw * s);
     }
 
-  // define other methods down here.
+    line(xStartPos, yStartPos, (float) xDraw, (float) yDraw);  
+
+  // for some reason, it doesn't stop stuff when yDraw is < 0
     if (xDraw > 800 || yDraw < 0) {
-      stop = true;
-        while (stop == true)
-          xDraw = 0;
+      stop = true;      
     }
-*/
+    while (stop == true){
+      xDraw = 0;
+      stop = false;
+    }
 
   }
 }
